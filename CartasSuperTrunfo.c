c675cfa85a3165a566034dc3e6bcf60b326f6083
RengawSM <wagnersmith43@gmail.com>
2025-02-26 03:25:05 +0000

    Correções de nomes

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,26 +2,32 @@
 
 int main(){
 
-    printf("SUPER TRUNFO: CADASTRO DE CARTAS! \n");
 
-    char estado, nome, codigo;
+    char estado, nome;
+    char codigo;
     int populacao, pontos_turisticos;
     float area, PIB;
     
+ printf("SUPER TRUNFO: CADASTRO DE CARTAS! \n");
 
-    printf("Vamos lá! \n");
-    printf("Insira o nome do Estado referente a esta carta: \n");
-    
-    scanf("%s", &estado);
+ printf("Vamos lá! \n");
+ printf("Insira o nome do Estado referente a esta carta: \n");
+scanf("%s", &estado);
 
-    printf("Insira o nome de uma cidade deste estado: \n");
-    scanf("%s", &nome);
-    
-    printf("insira o código da cidade: \n");
-    scanf("%s", codigo);
-    
-    printf("insira a população desta cidade \n");
-    scanf("%d", &populacao);
+printf("Insira o nome de uma cidade deste estado: \n");
+ 
+scanf("%s", &nome);
+
+printf("insira o código da cidade: \n");
+scanf("%s", &codigo);
+
+printf("insira a população desta cidade \n");
+scanf("%d", &populacao);
 
     return 0;
+
+
+
 }
+
+
