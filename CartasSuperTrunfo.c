847da5cc928dad64ba6ac20e8d975346a52ae0f6
RengawSM <wagnersmith43@gmail.com>
2025-02-28 03:48:03 +0000

    Feito porém com falhas ao rodar

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,9 +2,10 @@
 
 int main(){
 
+// Não estou conseguindo achar o erro que está impedindo a rodagem, pode me ajudar?
 
-    char estado, estado2 cidade;
-    char codigo, codigo2;
+    char estado[50], estado2[50], cidade[50], cidade2[50];
+    char codigo[10], codigo2[10];
     float populacao, populacao2; 
     int pontos_turisticos, pontos_turisticos2;
     float area, area2, PIB, PIB2;
@@ -14,14 +15,14 @@ int main(){
  printf("Vamos lá! \n");
 
  printf("Insira o nome do Estado referente a esta carta: \n");
-scanf("%s", &estado);
+scanf("%s", estado);
 
 printf("Insira o nome de uma cidade deste estado: \n");
  
-scanf("%s", &cidade);
+scanf("%s", cidade);
 
 printf("insira o código da carta: \n");
-scanf("%s", &codigo);
+scanf("%s", codigo);
 
 printf("insira a população desta cidade \n");
 scanf("%f", &populacao);
@@ -30,7 +31,7 @@ printf("Agora insira a área em km² \n");
 scanf("%f", &area);
 
 printf("Qual o PIB desta cidade? \n");
-scanf("%F", &PIB);
+scanf("%f", &PIB);
 
 printf("Agora quantos pontos turísticos há nesta cidade? \n");
 scanf("%d", &pontos_turisticos);
@@ -38,23 +39,50 @@ scanf("%d", &pontos_turisticos);
 printf("Muito bem! Vamos à próxima carta: \n");
 
 printf("Insira o estado da carta 2: \n");
-scanf("%s", &estado2);
+scanf("%s", estado2);
 
 printf("Agora uma cidade deste estado: \n");
-scanf("%s", &cidade2);
+scanf("%s", cidade2);
 
 printf("Insira o código da carta: \n");
-scanf("%s, &codigo2");
+scanf("%s", codigo2);
 
 printf("Agora a população desta cidade: \n");
 scanf("%f", &populacao2);
 
+printf("Insira a área desta cidade em km² \n");
+scanf("%f", &area2);
+
+printf("Insira o PIB desta cidade \n");
+scanf("%f", &PIB2);
+
 printf("Agora quantos pontos turístico há nesta cidade: \n");
 scanf("%d", &pontos_turisticos2);
-    return 0;
+
+printf("Muito bem! As cartas cadastradas foram as seguntes: \n");
+printf("Carta 1: \n");
+printf("Estado: %s \n", estado);
+printf("Código: %s \n", codigo);
+printf("Nome da cidade: %s \n", cidade);
+printf("População: %d \n", populacao);
+printf("Área: %d \n", area);
+printf("PIB: %d \n", PIB);
+printf("Pontos turísticos: %d \n", pontos_turisticos);
+
+printf("Carta 2: \n");
+printf("Estado: %s \n", estado2);
+printf("Código: %s \n", codigo2);
+printf("Nome da cidade: %s \n", cidade2);
+printf("Popilação: %d \n", populacao2);
+printf("Área: %d \n", area2);
+printf("PIB: %d \n", PIB2);
+printf("Pontos turísticos: %d \n", pontos_turisticos2);
+
+
+
+return 0;
 
 
 
 }
-
 
